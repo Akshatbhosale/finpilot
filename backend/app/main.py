371890d9d4eb2215d6d3615c7287dc56a501de11
Akshatbhosale <bhosaleakshat@gmail.com>
2026-02-11 22:32:33 +0530

    feat:connect FastAPI to postgreSQL

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,7 +1,12 @@
 from fastapi import FastAPI
+from app.core.database import engine, Base
 
 app = FastAPI(title="FinPilot API")
 
+@app.on_event("startup")
+def startup():
+    Base.metadata.create_all(bind=engine)
+
 @app.get("/")
 def root():
-    return {"message": "FinPilot backend running"}
+    return {"message": "FinPilot backend running with DB"}
